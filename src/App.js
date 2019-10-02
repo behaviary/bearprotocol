b0bdfed9d2c53918fafd327d838b75212fe6aeee
Peter <peter@nearprotocol.com>
2019-10-02 16:11:47 -0700

    bear

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>bear optimized protocol</h1>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/1200px-2010-kodiak-bear-1.jpg"/>
+      <img src="https://peopledotcom.files.wordpress.com/2019/09/grizzly-bear.jpg"/>
+      <img src="https://i.guim.co.uk/img/media/11d4c182d094199e26ddb36febe67123a9bbc93a/34_246_2966_4275/master/2966.jpg?width=700&quality=85&auto=format&fit=max&s=e78fc63cf43a4a9830619c324b5c854b"/>
+      <img src="https://images.unsplash.com/photo-1530595467537-0b5996c41f2d?ixlib=rb-1.2.1&w=1000&q=80"/>
     </div>
   );
 }
